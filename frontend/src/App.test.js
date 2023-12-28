d5ef9aadfaf6564058d709e451bc01edbe286af4
jangminjuu <mj.ran91@gmail.com>
2023-12-29 01:01:56 +0900

    apptest.js file code update

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders App component', () => {
-  // App 컴포넌트 렌더링
   render(<App />);
-  
-  // 예상하는 요소 또는 특정 조건을 확인하는 코드를 작성
-  // ex. 특정 텍스트가 렌더링되는지 확인하는 코드
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
