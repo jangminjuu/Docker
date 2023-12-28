fd5a76df3126e9c6a9351f753998ce5f29372852
jangminjuu <mj.ran91@gmail.com>
2023-12-29 00:34:36 +0900

    test code added'

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App component', () => {
+  // App 컴포넌트 렌더링
+  render(<App />);
   
+  // 예상하는 요소 또는 특정 조건을 확인하는 코드를 작성
+  // ex. 특정 텍스트가 렌더링되는지 확인하는 코드
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
